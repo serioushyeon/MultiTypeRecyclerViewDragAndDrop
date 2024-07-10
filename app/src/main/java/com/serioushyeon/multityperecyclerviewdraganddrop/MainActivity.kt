774aecba6bd7a619f5f8016e15545c83bb3d5e2d
Jihyeon <zz64446949@gmail.com>
2024-07-10 15:06:21 +0900

    Test: 테스트 더미데이터 변경

--- app/src/main/java/com/serioushyeon/multityperecyclerviewdraganddrop/MainActivity.kt
+++ app/src/main/java/com/serioushyeon/multityperecyclerviewdraganddrop/MainActivity.kt
@@ -31,17 +31,14 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initMultiTypeRecyclerView() {
         binding.rvScript.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        items.add(MultiTypeItem.PPTButtonItem)
         items.add(MultiTypeItem.TextItem("안녕하세요 여러분, 오늘 제가 여러분께 소개할 주제는 'iOS 프로젝트 주제 추천'입니다."))
         items.add(MultiTypeItem.TextItem("이 주제를 통해 Swift 언어를 사용하여 단기간에 개발할 수 있는 앱 개발 프로젝트에 대해 알아보겠습니다."))
-        items.add(MultiTypeItem.PPTButtonItem)
         items.add(MultiTypeItem.TextItem("우선, iOS 앱 개발에 있어서 Swift는 매우 중요한 언어입니다."))
         items.add(MultiTypeItem.TextItem("Swift는 Apple이 iOS, macOS, watchOS, tvOS를 위해 만든 프로그래밍 언어로, 간결하면서도 강력한 기능을 제공합니다."))
         items.add(MultiTypeItem.TextItem("Swift를 사용함으로써 개발자는 보다 빠르고 안정적인 앱을 개발할 수 있습니다."))
         items.add(MultiTypeItem.TextItem("다음으로, 단기간 개발을 위한 프로젝트 아이디어를 몇 가지 소개하겠습니다."))
         items.add(MultiTypeItem.TextItem("첫 번째 아이디어는 '일기장 앱'입니다. 사용자가 매일의 일기를 쓰고 사진을 첨부할 수 있는 간단한 앱입니다."))
         items.add(MultiTypeItem.TextItem("이 프로젝트는 데이터 저장과 UI 디자인의 기본을 다루기 때문에 초보 개발자에게 적합합니다."))
-        items.add(MultiTypeItem.BreathButtonItem)
         val multiTypeRecyclerViewAdapter = RecyclerViewAdapter(items)
         binding.rvScript.adapter = multiTypeRecyclerViewAdapter
     }
